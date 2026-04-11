2277c3434fa0f23521e2fb4c3fe80e66a38cd337
Prithivi <prithivi@users.noreply.github.com>
2026-04-12 01:00:58 +0545

    feat(server): set up Express server with middleware and health check endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+import "dotenv/config";
+import express, { type Express, type Request, type Response } from "express";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import { Env } from "./config/env.config";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/async-handler.middleware";
+import { errorHandler } from "./middlewares/error-handler.middleware";
+import { connectDatabase } from "./config/database.config";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(
+  cors({
+    origin: Env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+app.use(morgan("dev"));
+
+app.get(
+  "/health",
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Server is healthy",
+      status: "OK",
+    });
+  }),
+);
+
+app.use(errorHandler);
+
+app.listen(Env.PORT, async () => {
+  await connectDatabase();
+  console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
+});
