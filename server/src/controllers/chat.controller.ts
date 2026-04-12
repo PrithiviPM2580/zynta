999182d420a559e88cd0db43b9ed407116d4e8f9
Prithivi <prithivi@users.noreply.github.com>
2026-04-12 14:46:56 +0545

    feat(chat): add getChatController to retrieve chat and messages by ID

--- server/src/controllers/chat.controller.ts
+++ server/src/controllers/chat.controller.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/async-handler.middleware";
 import { HTTPSTATUS } from "../config/http.config";
-import { createChatSchema } from "../validators/chat.validator";
+import { chatIdSchema, createChatSchema } from "../validators/chat.validator";
 import {
   createChatService,
   getUserChatsService,
+  getChatService,
 } from "../services/chat.service";
 
 export const createChatController = asyncHandler(
@@ -30,6 +31,21 @@ export const getUserChatsController = asyncHandler(
     return res.status(HTTPSTATUS.OK).json({
       message: "User chats retrieved successfully",
       chat,
+    });
+  },
+);
+
+export const getChatController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+    const { id } = chatIdSchema.parse(req.params);
+
+    const { chat, messages } = await getChatService(userId, id);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Chat retrieved successfully",
+      chat,
+      messages,
     });
   },
 );
