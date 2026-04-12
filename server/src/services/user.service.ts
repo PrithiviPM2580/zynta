11c06ada48bd81f337033c266e5d3f09bb8b1e45
Prithivi <prithivi@users.noreply.github.com>
2026-04-12 13:15:11 +0545

    feat(user): add getUsersService to retrieve users excluding the current user

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -3,3 +3,10 @@ import User from "../models/user.model";
 export const findByIdUserService = async (userId: string) => {
   return User.findById(userId).lean();
 };
+
+export const getUsersService = async (userId: string) => {
+  const users = await User.find({ _id: { $ne: userId } })
+    .select("-password")
+    .lean();
+  return users;
+};
