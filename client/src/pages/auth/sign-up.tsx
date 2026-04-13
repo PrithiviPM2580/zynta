1398e279e8154d3b6a7496071f152283a8af4bd2
Prithivi <prithivi@users.noreply.github.com>
2026-04-13 23:15:31 +0545

    feat(sign-up): implement sign-up form with validation and user input handling

--- client/src/pages/auth/sign-up.tsx
+++ client/src/pages/auth/sign-up.tsx
@@ -1,7 +1,100 @@
-import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useAuth } from "@/hooks/use-auth";
+import { registerSchema, type RegisterSchema } from "@/validator/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { FaFacebookMessenger } from "react-icons/fa";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const { register, isSigningUp } = useAuth();
+
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: RegisterSchema) => {
+    if (isSigningUp) return;
+    register(values);
+  };
+
+  return (
+    <div className="flex min-h-svh justify-center items-center bg-muted p-6">
+      <div className="w-full max-w-sm">
+        <Card>
+          <CardHeader className="flex flex-col items-center justify-center gap-3">
+            <FaFacebookMessenger />
+            <CardTitle className="text-xl">Create your account</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="grid gap-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="John Doe" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="john.doe@example.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="••••••••"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
