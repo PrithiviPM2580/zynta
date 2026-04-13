3d48e25f589d0d0de76a33e8cc677f0a47bcc806
Prithivi <prithivi@users.noreply.github.com>
2026-04-13 23:15:27 +0545

    feat(validator): add registration schema validation using Zod

---
+++ client/src/validator/auth.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
