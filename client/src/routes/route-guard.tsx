7c2b0aa9f5134ae518c45b46aa9cb6029b60bbf9
Prithivi <prithivi@users.noreply.github.com>
2026-04-14 12:47:03 +0545

    feat(auth): enhance RouteGuard to navigate based on authentication state

--- client/src/routes/route-guard.tsx
+++ client/src/routes/route-guard.tsx
@@ -1,12 +1,18 @@
+import { useAuth } from "@/hooks/use-auth";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 interface RouteGuardProps {
   requireAuth?: boolean;
 }
 
 const RouteGuard = ({ requireAuth }: RouteGuardProps) => {
-  console.log("RouteGuard: requireAuth =", requireAuth);
+  const { user } = useAuth();
+
+  if (requireAuth && !user) return <Navigate to="/" replace />;
+
+  if (!requireAuth && user) return <Navigate to="/chat" replace />;
+
   return <Outlet />;
 };
 
