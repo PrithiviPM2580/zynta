27b84bff2e359ff7ef5db67119399ddf231cb3f6
Prithivi <prithivi@users.noreply.github.com>
2026-04-14 12:49:45 +0545

    feat(auth): improve loading state handling during authentication check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,20 @@ import { useEffect } from "react";
 import { useAuth } from "./hooks/use-auth";
 import AppRoutes from "./routes";
 import { Spinner } from "./components/ui/spinner";
+import { useLocation } from "react-router-dom";
+import { isAuthRoute } from "./routes/routes";
 
 const App = () => {
   const { user, isAuthStatus, isAuthStatusLoading } = useAuth();
+  const { pathname } = useLocation();
+
+  const isAuth = isAuthRoute(pathname);
 
   useEffect(() => {
     isAuthStatus();
-  }, []);
+  }, [isAuthStatus]);
 
-  if (isAuthStatusLoading && !user) {
+  if (isAuthStatusLoading && !user && !isAuth) {
     return (
       <div className="w-full h-screen flex items-center justify-center">
         <Spinner />
