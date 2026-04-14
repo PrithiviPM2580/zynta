7ad189f9fe5a1439ee9334811a5c38c2fc3904d3
Prithivi <prithivi@users.noreply.github.com>
2026-04-14 20:54:50 +0545

    feat(helper): add getOtherUserGroup and formatChatTime functions for enhanced chat user handling

--- client/src/lib/helper.ts
+++ client/src/lib/helper.ts
@@ -1,7 +1,48 @@
 import { useSocket } from "@/hooks/use-socket";
+import type { ChatType } from "@/types/chat.type";
+import { format, isThisWeek, isToday, isYesterday } from "date-fns";
 
 export const isUserOnline = (userId?: string) => {
   if (!userId) return false;
   const { onlineUsers } = useSocket.getState();
   return onlineUsers.includes(userId);
+};
+
+export const getOtherUserGroup = (
+  chat: ChatType,
+  currentUserId: string | null,
+) => {
+  const isGroup = chat?.isGroup;
+
+  if (isGroup) {
+    return {
+      name: chat?.groupName || "Unnamed Group",
+      subHeading: `${chat?.participants.length || 0} members`,
+      avatar: "",
+      isGroup,
+    };
+  }
+
+  const other = chat?.participants.find((p) => p._id !== currentUserId);
+  const isOnline = isUserOnline(other?._id ?? "");
+
+  return {
+    name: other?.name || "Unknown",
+    subHeading: isOnline ? "Online" : "Offline",
+    avatar: other?.avatar || "",
+    isGroup,
+    isOnline,
+  };
+};
+
+export const formatChatTime = (date: string | Date) => {
+  if (!date) return "";
+  const newDate = new Date(date);
+
+  if (isNaN(newDate.getTime())) return "Invalid date";
+
+  if (isToday(newDate)) return format(newDate, "h:mm a");
+  if (isYesterday(newDate)) return "Yesterday";
+  if (isThisWeek(newDate)) return format(newDate, "EEEE");
+  return format(newDate, "M/d");
 };
