5a08c1a313c971f1085b68012a8c08bf2f2f2f09
Prithivi <prithivi@users.noreply.github.com>
2026-04-14 12:47:06 +0545

    feat(auth): implement loading state with spinner during authentication check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { useAuth } from "./hooks/use-auth";
 import AppRoutes from "./routes";
+import { Spinner } from "./components/ui/spinner";
 
 const App = () => {
+  const { user, isAuthStatus, isAuthStatusLoading } = useAuth();
+
+  useEffect(() => {
+    isAuthStatus();
+  }, []);
+
+  if (isAuthStatusLoading && !user) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
   return <AppRoutes />;
 };
 
