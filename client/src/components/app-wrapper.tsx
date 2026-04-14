5b7e4d15cd0e62693c59abbc1d37f20b8f6bfccd
Prithivi <prithivi@users.noreply.github.com>
2026-04-14 18:32:41 +0545

    feat(app): add AsideBar component to AppWrapper for improved layout

--- client/src/components/app-wrapper.tsx
+++ client/src/components/app-wrapper.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import AsideBar from "./aside-bar";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
+      <AsideBar />
       <main className="h-full">{children}</main>
     </div>
   );
