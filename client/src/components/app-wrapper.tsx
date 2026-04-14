b8cc3790b4b62c59d816a0f8b5319d49debd8c19
Prithivi <prithivi@users.noreply.github.com>
2026-04-14 18:58:08 +0545

    feat(app-wrapper): enhance layout with background and padding adjustments

--- client/src/components/app-wrapper.tsx
+++ client/src/components/app-wrapper.tsx
@@ -3,9 +3,9 @@ import AsideBar from "./aside-bar";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="h-full min-h-dvh bg-background">
       <AsideBar />
-      <main className="h-full">{children}</main>
+      <main className="h-full pb-16 md:pb-0 md:pl-16">{children}</main>
     </div>
   );
 };
